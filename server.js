d6e6f9c670a80f8045af0bc143d32062a16728b4
mameow <mameow.spmm@gmail.com>
2023-02-15 17:46:39 +0700

    Authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -10,14 +11,19 @@ connectDB();
 
 //Route files
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 
 const app = express();
 
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 //Mount routers
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 10000;
 
